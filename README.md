a5cbc2901762f223672cdaa0ad6c09294cc1d01e
Joe Chasinga <jo.chasinga@gmail.com>
2017-09-20 12:02:20 -0700

    I'm pretty sure it's good enough to read bf characters except '[' and ']'

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
-# BrainfuckJS
-When you feel your tinker-tooth aching, write some BF in your JS.
+# ml-brainfuck
+When you feel your tinker-tooth aching, interpret Brainfuck in an ML.
 
-# Usage
+## Usage
+Right now it doesn't do much apart from reading a text file and incrementing/decrementing
+value in specified cells and move between cells. Looping is to be implemented.
 
+## Goal
++ Looping
++ Read from command line and file
++ Include Bucklescript backend 
++ Embed in Javascript
++ Spit out [dad jokes](1)
+
+[1]: https://www.buzzfeed.com/mikespohr/29-dad-jokes-that-are-so-bad-their-actually-good?utm_term=.ebNRlqoAQ#.dwdxgb3Xa
 Include Brainfuck expression in JavaScript:
+
+**Some idea from Flow**: 
+
+Embed in JS
 
 ```javascript
 
@@ -12,14 +26,6 @@ let helloworld = () => {
   let bfgreet = <<<<++++>>>.;
   console.log(bfgreet);
 };
-
-```
-
-then 
-
-```bash
-
-$ bf run helloworld.js
 
 ```
 
--- brainfuck.ml
+++ brainfuck.ml
@@ -1,5 +1,4 @@
 open Printf
-open Char
 
 let inc tape pos = 
   let value = tape.(pos) + 1 in tape.(pos) <- value; tape
@@ -7,6 +6,7 @@ let inc tape pos =
 let dec tape pos = 
   let value = tape.(pos) - 1 in tape.(pos) <- value; tape
 
+(* interpret accepts an optional argument to set the tape size *)
 let interpret ?(tape_size = 30000) code = 
   let tape = Array.make tape_size 0 in
   let rec scan code_pos tape_pos tape result = 
@@ -15,21 +15,17 @@ let interpret ?(tape_size = 30000) code =
       let cc = code.[code_pos] in
       (* printf "code_pos: %d | char: %c\n" code_pos cc; *)
       if cc = '+' then 
-        let tp = inc tape tape_pos in 
-          scan (code_pos+1) tape_pos tp result
+        let tp = inc tape tape_pos in scan (code_pos+1) tape_pos tp result
       else if cc = '-' then 
-        let tp = dec tape tape_pos in 
-          scan (code_pos+1) tape_pos tp result
+        let tp = dec tape tape_pos in scan (code_pos+1) tape_pos tp result
       else if cc = '>' then 
-        let tp_pos = tape_pos + 1 in
-          scan (code_pos+1) tp_pos tape result
+        let tp_pos = tape_pos + 1 in scan (code_pos+1) tp_pos tape result
       else if cc = '<' then 
-        let tp_pos = tape_pos - 1 in
-          scan (code_pos+1) tp_pos tape result
+        let tp_pos = tape_pos - 1 in scan (code_pos+1) tp_pos tape result
       else if cc = '.' then 
         let tp_pos = tape_pos in 
-          print_int tape.(tp_pos);
-          scan (code_pos+1) tp_pos tape result
+        print_int tape.(tp_pos);
+        scan (code_pos+1) tp_pos tape result
       else
         scan (code_pos+1) tape_pos tape result 
   in scan 0 0 tape ""
@@ -56,10 +52,14 @@ let () =
   if Array.length Sys.argv > 2 then
     print_string "You're too greedy! One at a time!"; print_newline ()
 
-let explode s = 
-  let rec exp i l =
-    if i < 0 then l 
-    else exp (i - 1) (s.[i] :: l) 
-  in exp (String.length s - 1) []
+(* Not used, but may be useful later 
+  (* 
+    let explode s = 
+      let rec exp i l =
+        if i < 0 then l 
+        else exp (i - 1) (s.[i] :: l) 
+      in exp (String.length s - 1) []
+  *)
+*)
 
 
