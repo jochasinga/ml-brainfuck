8a417204236c172e4c46ece8fa5d32b22a4a10b7
Joe Chasinga <jo.chasinga@gmail.com>
2017-09-28 00:43:41 -0700

    Implement loop; Nested loop broken; Add manual tests

--- brainfuck.ml
+++ brainfuck.ml
@@ -1,33 +1,57 @@
 open Printf
 
+(* `inc` increment tape value *)
 let inc tape pos = 
   let value = tape.(pos) + 1 in tape.(pos) <- value; tape
 
+(* `dec` decrements tape value *)
 let dec tape pos = 
   let value = tape.(pos) - 1 in tape.(pos) <- value; tape
 
-(* interpret accepts an optional argument to set the tape size *)
+(* `eat_to_next` eat up tokens until the next_char *)
+let rec eat_to_next code code_pos next_char =
+  let token = code.[code_pos] in
+  if token = next_char then code_pos else eat_to_next code (code_pos+1) next_char
+  
+(* `interpret` accepts an optional argument to set the tape size *)
 let interpret ?(tape_size = 30000) code = 
   let tape = Array.make tape_size 0 in
-  let rec scan code_pos tape_pos tape result = 
-    if code_pos >= String.length code then print_string result
-    else
+  let right_sqbrack_pos = ref 0 in
+  let rec scan code_pos tape_pos tape result =
+    if code_pos >= String.length code then print_string result else
       let cc = code.[code_pos] in
+      
       (* printf "code_pos: %d | char: %c\n" code_pos cc; *)
-      if cc = '+' then 
+      if cc = '[' then (
+        right_sqbrack_pos := code_pos;
+        if tape.(tape_pos) <> 0 then scan (code_pos+1) tape_pos tape result else
+          let new_pos = eat_to_next code code_pos ']' in
+          scan (new_pos) tape_pos tape result )
+          
+      else if cc = ']' then
+        if tape.(tape_pos) <> 0 then scan !right_sqbrack_pos tape_pos tape result else
+          scan (code_pos+1) tape_pos tape result
+                                
+      else if cc = '+' then 
         let tp = inc tape tape_pos in scan (code_pos+1) tape_pos tp result
+                                    
       else if cc = '-' then 
         let tp = dec tape tape_pos in scan (code_pos+1) tape_pos tp result
+                                    
       else if cc = '>' then 
         let tp_pos = tape_pos + 1 in scan (code_pos+1) tp_pos tape result
+                                   
       else if cc = '<' then 
         let tp_pos = tape_pos - 1 in scan (code_pos+1) tp_pos tape result
+                                   
       else if cc = '.' then 
-        let tp_pos = tape_pos in 
+        let tp_pos = tape_pos in
         print_int tape.(tp_pos);
         scan (code_pos+1) tp_pos tape result
+        
       else
-        scan (code_pos+1) tape_pos tape result 
+        scan (code_pos+1) tape_pos tape result
+      
   in scan 0 0 tape ""
 
 let () =
